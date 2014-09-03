de5248840975f422a42f7b1b764d00591fa49600
wangqing <zhishan99@163.com>
2014-09-03 10:55:08 +0800

    devv

--- hunt4j-server/src/main/java/com/yermoon/server/dao/UrlHashDao.java
+++ hunt4j-server/src/main/java/com/yermoon/server/dao/UrlHashDao.java
@@ -3,11 +3,9 @@ package com.yermoon.server.dao;
 import com.yermoon.server.entity.HashCodeEntity;
 
 /**
- * Created with IntelliJ IDEA.
- * User: wangq
- * Date: 14-5-17
- * Time: 下午8:38
  * To change this template use File | Settings | File Templates.
+ * @author wangqing
+ * @since 1.0.0
  */
 public interface UrlHashDao {
 
--- hunt4j-server/src/main/java/com/yermoon/server/dao/UrlTaskDao.java
+++ hunt4j-server/src/main/java/com/yermoon/server/dao/UrlTaskDao.java
@@ -6,11 +6,9 @@ import com.yermoon.server.entity.UrlTask;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: wangq
- * Date: 14-5-17
- * Time: 上午11:54
  * To change this template use File | Settings | File Templates.
+ * @author wangqing
+ * @since 1.0.0
  */
 public interface UrlTaskDao {
 
